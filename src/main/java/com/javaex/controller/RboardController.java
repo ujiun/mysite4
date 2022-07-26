7c81a6e180f73fad5b376f0ac2ef86f8146c1ae0
ujiun <ujiun@hanmail.net>
2022-07-26 23:19:08 +0900

    mysite4

--- src/main/java/com/javaex/controller/RboardController.java
+++ src/main/java/com/javaex/controller/RboardController.java
@@ -3,12 +3,12 @@ package com.javaex.controller;
 import org.springframework.stereotype.Controller;
 
 //계층형게시판
-@Controller
+@Controller 
 public class RboardController {
 
 	public void test() {
 		System.out.println("테스트");
 		
 	}
-	
+	 
 }
